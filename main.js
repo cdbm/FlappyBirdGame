8047be875f965480c23fa5bac4fe20b596a61dee
cdbm <cdbm@cin.ufpe.br>
2019-09-02 13:31:55 -0300

    add colisions and score

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ var mainState = {
 
         this.pipes = game.add.group();
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+        this.score = 0;
+        this.labelScore = game.add.text(20,20,"0", {font: "30px Arial", fill: "#ffffff"});
     },
 
     update: function() {
@@ -30,6 +32,9 @@ var mainState = {
         // It contains the game's logic   
         if (this.bird.y < 0 || this.bird.y > 490)
         this.restartGame();
+
+        game.physics.arcade.overlap(
+            this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
@@ -56,6 +61,8 @@ var mainState = {
         // Randomly pick a number between 1 and 5
         // This will be the hole position
         var hole = Math.floor(Math.random() * 5) + 1;
+        this.score += 1;
+        this.labelScore.text = this.score; 
     
         // Add the 6 pipes 
         // With one big hole at position 'hole' and 'hole + 1'
